47f2dd77db55a2b47b5bb9f80baf6ed461f988d1
Saptadipa Roy <saptadiparoy.777@gmail.com>
2026-06-11 00:38:03 +0530

    updating code
    Updated the number guessing game logic and messages.

--- numberguess.py
+++ numberguess.py
@@ -1,36 +1,49 @@
-import random as rnd
+#number guessing game
+#initialise range
+import random as rdm
 
-num_to_guess = rnd.randint(0,100)
+picked_number = rdm.randint(0,99)
+print(picked_number)
 
-print (num_to_guess)
+#welcome message and legend
 
+print ("Hello! Welcome to the number guessing game! The rules are simple, the system has picked a number and you need to guess it.")
+print ("when your guess is around 5 units or closer to the number - Very Warm \nwhen guess is 15 units or closer to number - warm" \
+"\nwhen guess is 25 units or closer to number - cold \nwhen guess is 35 units or closer to number - Colder \nfarther than 35 units - way too cold.")
+
+
+#creating an empty list that stores guesses
 guesses = [0]
 
+#running a loop for main game
 while True:
-    user_guess = int(input("enter a number to guess between (0,100):    "))
+    #takes guess input in integer form
+    guess_num = int(input("Guess the number! (0-99):    "))
 
-    if user_guess > 100 :
-        print("Please guess withing 0 to 100!")
+    #first check to ensure guess is within range
+    if guess_num > 99 or guess_num < 0:
+        print("Guess within Range!")
 
-    guesses.append(user_guess)
+    #appending guess to earlier made list to store and count number of guesses
+    guesses.append(guess_num)
 
-    if user_guess == num_to_guess:
-            print (f"You got it!! the number was {num_to_guess}")
-            print (f"number of guesses ---- >{(len(guesses)-1)}")
-            break
+    #conditions according to legend/rules, comparing number and checking
+    if guess_num == picked_number:
+        print (f"You won! The number was {picked_number}")
+        print (f"It took you {len(guesses)-1} guesses")
+        print (guesses)
 
-    if guesses[-2]:
-        if abs (num_to_guess - user_guess) < abs(guesses[-2]):
-             print ("VERY WARM")
+    elif abs(guess_num - picked_number) <= 5:
+        print ("Very Warm!")
 
-        else:
-             print ("COLDER")
-    
-    else :
-        if abs(num_to_guess - user_guess) <= 10:
-              print ("warmm")
+    elif abs(guess_num - picked_number) <= 15:
+        print ("warm!")
 
-        else:
-             print ("cold")
+    elif abs(guess_num - picked_number) <= 25:
+        print ("cold!")
 
-print (guesses)
+    elif abs(guess_num - picked_number) <= 35:
+        print ("colder!")
+
+    else:
+        print ("WAYY TOO COLD!!!")
